cf63d581b5975949e0d517d0e008e905e406458c
joanedna1183 <joanedna11@gmail.com>
2026-01-13 13:00:01 +0700

    Initial commit

---
+++ Test1/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace NamaProyek
+{
+ class Program
+   {
+       static void Main(string[] args)
+       {
+            int n = 15;
+            for (int i=1; i<=n; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine("foo");
+                } else if (i%5 == 0)
+                {
+                    Console.WriteLine("bar");
+                }
+            }
+            
+       }
+   }
+}
